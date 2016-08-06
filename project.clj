aa067f6143821536e3b4142b076dc2cee1e4730e
Jason Murphy <json.murphy@gmail.com>
2016-08-05 19:27:17 -0500

    Added url, license and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ck_app/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Leiningen template for Conskit applications"
+  :url "https://github.com/conskit/ck_app"
+  :license {:name "Apache License, Version 2.0"
+                        :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :eval-in-leiningen true)
