100ce7b56d8b77371ec1b8677f2dd03ddf2f5dbd
Jason Murphy <jmurphy@jpsco.com>
2017-05-11 10:26:19 -0500

    Added route-info method Fixes #2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ck_app/lein-template "0.1.6"
+(defproject ck_app/lein-template "0.1.7"
   :description "Leiningen template for Conskit applications"
   :url "https://github.com/conskit/ck_app"
   :license {:name "Apache License, Version 2.0"
--- resources/leiningen/new/ck_app/user.clj
+++ resources/leiningen/new/ck_app/user.clj
@@ -1,11 +1,13 @@
- (ns user
-   (:require [clojure.pprint :as pprint]
-             [clojure.tools.namespace.repl :refer [refresh]]
-             [puppetlabs.trapperkeeper.app :as tka]
-             [puppetlabs.trapperkeeper.bootstrap :as bootstrap]
-             [puppetlabs.trapperkeeper.config :as config]
-             [puppetlabs.trapperkeeper.core :as tk]
-             [figwheel-sidecar.repl-api :as f]))
+(ns user
+  (:require [clojure.pprint :as pprint]
+            [clojure.tools.namespace.repl :refer [refresh]]
+            [puppetlabs.trapperkeeper.app :as tka]
+            [puppetlabs.trapperkeeper.bootstrap :as bootstrap]
+            [puppetlabs.trapperkeeper.config :as config]
+            [puppetlabs.trapperkeeper.core :as tk]
+            [conskit.protocols :as ckp]
+            [figwheel-sidecar.repl-api :as f])
+  (:import (java.util.regex Pattern)))
 
 (def system nil)
 
@@ -38,3 +40,16 @@
 
  (defn cljs-repl []
    (f/cljs-repl))
+
+(defn route-info
+  [route-str]
+  (if system
+    (let [routes (get-in @(tka/app-context system) [:service-contexts :CKRouter :routes])
+          registry (tka/get-service system :ActionRegistry)
+          pred #(let [route (:route %)]
+                  (if (= (type route) Pattern)
+                    (re-matches route route-str)
+                    (= route route-str)))
+          action-id (:id (first (filter pred routes)))]
+      (ckp/select-meta-keys registry action-id [:route :id :file :line :ns]))
+    (throw (Exception. "Instance could not be found. Please run (reset) first"))))
